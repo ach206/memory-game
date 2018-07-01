66c942bd9e2ace259ececae2da4cc8115facdbb5
Cami <camille@creativeambition.net>
2018-06-30 22:15:54 -0700

    modal displays correct seconds and moves

--- js/app.js
+++ js/app.js
@@ -5,29 +5,30 @@
 let cards = document.querySelector('.deck').childNodes;
 let allCards = document.getElementsByClassName('card');
 let cardNames = [
-    "diamond",
-    "paper-plane-o",
-    "anchor",
-    "bolt",
-    "cube",
+    "gem",
+    "barcode",
+    "kiss-wink-heart",
+    "rocket",
+    "user-ninja",
     "leaf",
-    "bicycle",
-    "bomb",
-    "diamond",
-    "paper-plane-o",
-    "anchor",
-    "bolt",
-    "cube",
+    "meh-rolling-eyes",
+    "battery-full",
+    "gem",
+    "barcode",
+    "kiss-wink-heart",
+    "rocket",
+    "user-ninja",
     "leaf",
-    "bicycle",
-    "bomb"
+    "meh-rolling-eyes",
+    "battery-full"
 ];
 let deck = document.querySelector('.deck');
 let cardsClickedOn = [];
 let match = [];
 let deckHTML = " ";
 let time = document.querySelector('.timer');
-// let moves = document.querySelector('.moves').textContent;
+let moves = document.querySelector('.moves');
+let stars = document.getElementsByClassName('fa-star');
 /*
  * Display the cards on the page*/
 /*   - shuffle the list of cards using the provided "shuffle" method below
@@ -50,17 +51,17 @@ function shuffle(array) {
 
     return array;
 }
-//timer tracks user's time to complete
+//function displays time
 function startTimer() {
   time.textContent++;
 };
 //starting a new game
 function newGame() {
-    document.querySelector('.moves').textContent = 0;
+    moves.textContent = 0;
     let shuffledCards = shuffle(cardNames);
     cardNames.map(function(card) {
         deckHTML += `<li class="card">
-  <i class="fa fa-${card}"></i>
+  <i class="fas fa-${card}"></i>
   </li>`;
         return deckHTML;
     });
@@ -70,8 +71,9 @@ function newGame() {
 
 newGame();
 //when user clicks on the deck the game timer will start
+let timer = setInterval(startTimer, 1000);
 $(".deck").one("click", function() {
-  setInterval(startTimer, 1000);
+  //when let timer was inside here it worked but unfortunately it made my variable local scope and unable to clearInterval(timer)
 });
 
 //function will compare 2 cards to see if they are identical; if they match then the 2 matched cards are pushed into an array
@@ -91,7 +93,7 @@ function seeIfCardsMatch(evt) {
       cardsClickedOn[1].classList.remove('show', 'open');
       cardsClickedOn = [];
     }, 1000);
-    document.querySelector('.moves').textContent++;
+    moves.textContent++;
 }//close if else
 }; //closes seeIfCardsMatch fx
 
@@ -105,6 +107,7 @@ function showCards() {
           evt.target.classList.add("show", "open");
           seeIfCardsMatch();
           winner();
+          rating();
        } else {
          setTimeout(function() {
            cardsClickedOn[0].classList.remove('show', 'open');
@@ -119,11 +122,52 @@ function showCards() {
 
 showCards();
 
+function rating() {
+  if (moves > 25) {
+    stars[1].classList.remove("fa");
+    stars[2].classList.remove("fa");
+    stars[3].classList.remove("fa");
+    stars[4].classList.remove("fa");
+    stars[1].classList.add("far");
+    stars[2].classList.add("far");
+    stars[3].classList.add("far");
+    stars[4].classList.add("far");
+    text = "1 star";
+    return text;
+  } else if (moves > 18 && moves <= 25) {
+    stars[2].classList.remove("fa");
+    stars[3].classList.remove("fa");
+    stars[4].classList.remove("fa");
+    stars[2].classList.add("far");
+    stars[3].classList.add("far");
+    stars[4].classList.add("far");
+    text = "2 stars";
+    return text;
+  } else if (moves > 12 && moves <= 18) {
+    stars[3].classList.remove("fa");
+    stars[4].classList.remove("fa");
+    stars[3].classList.add("far");
+    stars[4].classList.add("far");
+    text = "3 stars";
+    return text;
+  } else if (moves > 10 && moves <= 12) {
+    stars[4].classList.remove("fa");
+    stars[4].classList.add("far");
+    text = "4 stars";
+    return text;
+  } else {
+    text = "5 stars";
+    return text;
+  } //ends if else
+} //closes rating function
+
+
 function winner (){
   let modal = document.querySelector('.modal');
   let modalText = document.querySelector('#modalText');
 if (match.length === 16) {
-  modalText.textContent = `Congrats! You found all the matches in  timer seconds and  moves moves. You scored  starCount stars!`;
+  clearInterval(timer);
+  modalText.textContent = `Congrats! You found all the matches in  ${time.textContent} seconds and  ${moves.textContent} moves. You scored  ${rating(text)} stars!`;
   modal.style = "display: block;";
   console.log('you won');
 
